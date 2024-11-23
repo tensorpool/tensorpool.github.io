21802eeafae592a1cb2292ec69be151f764a6f51
Tycho Svoboda <tychosvoboda@gmail.com>
2024-11-22 17:17:23 -0800

    Updates

---
+++ precache-manifest.c807132621ec8667fda5712a3fd8a213.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6735eb63b8f850a0d92b16bf018edcff",
+    "url": "/tensorpool.github.io/index.html"
+  },
+  {
+    "revision": "ee888b5b91ffbf6a85f4",
+    "url": "/tensorpool.github.io/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "19b653926d9b087a2e07",
+    "url": "/tensorpool.github.io/static/js/2.5ff95bdd.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tensorpool.github.io/static/js/2.5ff95bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/tensorpool.github.io/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "ee888b5b91ffbf6a85f4",
+    "url": "/tensorpool.github.io/static/js/main.ace13ff9.chunk.js"
+  },
+  {
+    "revision": "b39119b2d9ce51591cb6",
+    "url": "/tensorpool.github.io/static/js/runtime-main.d23bacc4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tensorpool.github.io/static/media/logo.06e73328.svg"
+  }
+]);
