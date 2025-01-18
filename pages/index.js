c05194dadd3c667b46f96f31282507d6a3ad41cc
Tycho Svoboda <tychosvoboda@gmail.com>
2025-01-17 20:56:00 -0800

    one liner fix

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
 						TensorPool
 					</Heading>
 					<Text as="h2" fontSize={['xl', '2xl']} my={[6, 3]}>
-						Execute ML jobs on the cloud with no code.
+						The easiest way to execute ML jobs on the cloud.
 					</Text>
 					<Link href="/join">
 						<Button size="lg" bg="rpmblue" padding={6} my="4">
